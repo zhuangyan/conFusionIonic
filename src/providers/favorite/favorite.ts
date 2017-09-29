7abe653a8627a206e53f870c4578f7a034163d7d
zhuangyan <zy_php@163.com>
2017-09-29 16:14:04 +0800

    第三周作业

--- src/providers/favorite/favorite.ts
+++ src/providers/favorite/favorite.ts
@@ -4,6 +4,8 @@ import 'rxjs/add/operator/map';
 import { Dish } from '../../shared/dish';
 import { Observable } from 'rxjs/Observable';
 import { DishProvider } from '../dish/dish';
+import { Storage } from '@ionic/storage';
+
 
 /*
   Generated class for the FavoriteProvider provider.
@@ -17,14 +19,26 @@ export class FavoriteProvider {
     favorites: Array<any>;
   
     constructor(public http: Http,
-      private dishservice: DishProvider) {
+      private dishservice: DishProvider,
+      private storage:Storage) {
       console.log('Hello FavoriteProvider Provider');
-      this.favorites = [];
+      storage.get('favorites').then(favorites => {
+        if (favorites) {
+          console.log(favorites);
+          this.favorites = favorites;          
+        }
+        else{
+          console.log('favorites not defined');
+          this.favorites = [];
+        }
+          
+      });
     }
   
     addFavorite(id: number): boolean {
       if (!this.isFavorite(id))
         this.favorites.push(id);
+      this.storage.set('favorites', this.favorites)       
       console.log('favorites', this.favorites);
       return true;
     }
@@ -42,6 +56,7 @@ export class FavoriteProvider {
       let index = this.favorites.indexOf(id);
       if (index >= 0) {
         this.favorites.splice(index,1);
+        this.storage.set('favorites', this.favorites)
         return this.getFavorites();
       }
       else {
